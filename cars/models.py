a68f48da76011ef01ab2a3b2be5e1c5fda741d7a
Angelo Takao <shidehister@gmail.com>
2024-11-24 17:28:05 -0300

    atualização para o banco de dados car

--- cars/models.py
+++ cars/models.py
@@ -4,6 +4,9 @@ class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200, blank=True,null=True)
     brand = models.CharField(max_length=200)
-    factory_yer = models.IntegerField(blank=True, null=True)
+    factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.model
