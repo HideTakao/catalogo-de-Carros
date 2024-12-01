2fb793374fccc4b52304f00e88512392eb487460
Angelo Takao <shidehister@gmail.com>
2024-12-01 01:29:26 -0300

    adicionado filtros

--- app/settings.py
+++ app/settings.py
@@ -56,10 +56,10 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["app/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
-            "context_processors": [
+            "context_processors": [ 
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
