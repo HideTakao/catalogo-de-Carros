7707286d7d4409c8e061f588bf59a3893f5b0e56
Angelo Takao <shidehister@gmail.com>
2024-12-09 05:22:11 -0300

    criação deos templates para deletar e editar carros

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 class CarsListView(ListView):
@@ -26,3 +27,15 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
     
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk' : self.object.pk})
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/car_detail/'
