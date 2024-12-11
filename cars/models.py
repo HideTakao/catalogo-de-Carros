09d69094ecab37b855edcb919f5f32310ad16913
Angelo Takao <shidehister@gmail.com>
2024-12-11 05:49:50 -0300

    criação das signals e ajustes no template cars.html

--- cars/models.py
+++ cars/models.py
@@ -16,6 +16,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
@@ -27,7 +28,6 @@ class CarInventory(models.Model):
 
     class Meta:
         ordering = ['-created_at']
-
     def __str__(self):
         return f'{self.cars_count} - {self.cars_value}'
         
